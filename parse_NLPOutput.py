a20987c0477aa18dfe16ebdfb7e5169e1d6d9c62
ningvicky <wxn.chn@gmail.com>
2014-06-04 17:14:06 -0700

    Put in nlp xml files

---
+++ parse_NLPOutput.py
@@ -0,0 +1,15 @@
+def parse_xml(path):
+	f = open('/output/'+path, 'r')
+
+	i = 0
+	for i in range(1,100):
+		print(i)
+
+	# for line in f:
+	# 	if line == "vicky":
+	# 		something
+
+
+	# 	i += 1
+
+parse_xml("1.xml")
